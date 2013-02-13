20d5a2489af2870827788283f3a249dec92f52ae
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-02-13 17:57:22 -0500

    Remove the options list from the file comment
    It was already out of sync with the actual arguments the program uses
    and accepts.  It is easier for users to simply run the program with
    the ‘--help’ option than look at the source code as a manual.

--- get-code-bookmarks.py
+++ get-code-bookmarks.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 """
-get-code-bookmarks.py [-h] [-v] [--terms [TERMS [TERMS ...]]]
-                      [-o {normal,markdown,bbcode}]
-                      database
+get-code-bookmarks.py
 
 This program tries to extract all of the programming-related bookmarks
 from Firefox, or browsers based on the same platform (e.g. Conkeror).
